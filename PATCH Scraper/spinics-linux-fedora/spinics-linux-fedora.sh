91102e83bc232f1aadb3610e0d9ac57e8f958b7e
code-monk08 <mayank.singh081997@gmail.com>
2019-07-20 01:18:26 +0530

    modified scripts to find only patch urls and title

---
+++ PATCH Scraper/spinics-linux-fedora/spinics-linux-fedora.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+input="spinics-linux-fedora.txt"
+#reading url line by line from spinics-linux-fedora.txt
+while IFS= read -r line
+do 
+    #trimming *https://www.spinics.net/linux/fedora/* from url and making text file per mailing list
+    linkt="$line"
+    string2=${linkt#"https://www.spinics.net/linux/fedora/"}
+    string2=${string2%"/"}
+    #currently accepting all the urls of a page
+    lynx -width=1000 -list_inline -dump "$line" | grep "PATCH" > "$string2.txt"
+done < "$input"
